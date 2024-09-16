c2884936354a7e852adec8e95acd0b45c463c577
writedev <locxistwitch@gmail.com>
2024-09-16 13:18:52 +0200

    period test

--- test/app.py
+++ test/app.py
@@ -7,5 +7,10 @@ class Music:
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
 
-    def page(self):
+    def page(self, security_mode=False):
         print(self.soup.prettify())
+        if security_mode == False:
+            print("le mode securite est desactiver")
+
+        elif security_mode == True:
+            print("le mode securite est activer")
